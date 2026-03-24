028bf63b6d6ed8f31a2ac3cdd7538c6d3f7db141
emould2707 <emould@students.avc.edu>
2026-03-24 14:06:37 -0700

    a52 done for the second time but on my home pc because my laptop didnt wanna allow to save

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    // TODO
+    // TODO //It didnt save my work from my laptop because it wasnt able to push painnnn
+    int value, maxVal;
+    cin >> maxVal;
+    for (int i = 1; i < 10; i++){
+        cin >> value;
+        if (value > maxVal){
+            maxVal = value;
+        }
 
+    }
+    cout << maxVal << endl;
     return 0;
 }
